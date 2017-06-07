8dfe9eac18e543c761d6f420137452ae03a73202
Michael <micobb@outlook.com>
2017-06-07 08:15:31 -0500

    Added dist folder and more gulp tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,17 @@ var sass = require('gulp-sass');
 
 var browserSync = require('browser-sync').create();
 
+//Concats CSS/JS files if needed
+var useref = require('gulp-useref');
+
+var uglify = require('gulp-uglify');
+
+var gulpIf = require('gulp-if');
+
+var cssNano = require('gulp-cssnano');
+
+var del = require('gulp-del');
+
 //Run gulp sass plugin
 gulp.task('sass', function() {
 		return gulp.src('scss/**/*.scss')//Gets all the files ending with.scss in /scss and children dirs
@@ -32,3 +43,17 @@ gulp.task('watch', ['browserSync', 'sass'], function() {
 	   gulp.watch('js/**/*.js', browserSync.reload);
 })
 				
+
+//Concats files into single file!!
+gulp.task('useref', function() {
+      return gulp.src('*.html')
+	  .pipe(useref())
+	  // Minifies only if it's a Javascropt file
+	  .pipe(gulpIf('*.js', uglify()))
+	  .pipe(gulpIf('*.css', cssNano()))
+	  .pipe(gulp.dest('dist'))
+});
+
+gulp.task('del', function(){
+     return del.sync('dis');
+}))
